f2fb81f8330438ec4f86361dbe844df2354247e3
Kenji Okimoto <okimoto@clear-code.com>
2016-12-05 14:22:08 +0900

    Use new hash syntax
    rubocop -a --only Style/HashSyntax

--- lib/fluent/plugin/filter_geoip.rb
+++ lib/fluent/plugin/filter_geoip.rb
@@ -2,16 +2,16 @@ module Fluent
   class GeoipFilter < Filter
     Plugin.register_filter('geoip', self)
 
-    config_param :geoip_database, :string, :default => File.dirname(__FILE__) + '/../../../data/GeoLiteCity.dat'
-    config_param :geoip_lookup_key, :string, :default => 'host'
-    config_param :skip_adding_null_record, :bool, :default => false
+    config_param :geoip_database, :string, default: File.dirname(__FILE__) + '/../../../data/GeoLiteCity.dat'
+    config_param :geoip_lookup_key, :string, default: 'host'
+    config_param :skip_adding_null_record, :bool, default: false
 
     config_set_default :include_tag_key, false
 
-    config_param :hostname_command, :string, :default => 'hostname'
+    config_param :hostname_command, :string, default: 'hostname'
 
-    config_param :flush_interval, :time, :default => 0
-    config_param :log_level, :string, :default => 'warn'
+    config_param :flush_interval, :time, default: 0
+    config_param :log_level, :string, default: 'warn'
 
 
     def initialize
--- lib/fluent/plugin/out_geoip.rb
+++ lib/fluent/plugin/out_geoip.rb
@@ -5,20 +5,20 @@ require 'fluent/mixin'
 class Fluent::GeoipOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('geoip', self)
 
-  config_param :geoip_database, :string, :default => File.dirname(__FILE__) + '/../../../data/GeoLiteCity.dat'
-  config_param :geoip_lookup_key, :string, :default => 'host'
-  config_param :tag, :string, :default => nil
-  config_param :skip_adding_null_record, :bool, :default => false
+  config_param :geoip_database, :string, default: File.dirname(__FILE__) + '/../../../data/GeoLiteCity.dat'
+  config_param :geoip_lookup_key, :string, default: 'host'
+  config_param :tag, :string, default: nil
+  config_param :skip_adding_null_record, :bool, default: false
 
   include Fluent::HandleTagNameMixin
   include Fluent::SetTagKeyMixin
   config_set_default :include_tag_key, false
 
   include Fluent::Mixin::RewriteTagName
-  config_param :hostname_command, :string, :default => 'hostname'
+  config_param :hostname_command, :string, default: 'hostname'
 
-  config_param :flush_interval, :time, :default => 0
-  config_param :log_level, :string, :default => 'warn'
+  config_param :flush_interval, :time, default: 0
+  config_param :log_level, :string, default: 'warn'
 
   def configure(conf)
     super
